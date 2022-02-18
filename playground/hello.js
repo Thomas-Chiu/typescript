6e5113657b6af8eec7acfcbb2cdea63637a40316
ThomasChiu <ThomasChiu@hyenatek.com>
2022-02-18 14:10:36 +0800

    first init

---
+++ playground/hello.js
@@ -0,0 +1,4 @@
+var sayNameAndAge = function (name, age) {
+    return "I'm ".concat(name, " and I'm ").concat(age, " years old.");
+};
+console.log(sayNameAndAge("Thomas", 28));
---
+++ playground/hello.ts
@@ -0,0 +1,5 @@
+const sayNameAndAge = (name: string, age: number) => {
+  return `I'm ${name} and I'm ${age} years old.`;
+};
+
+console.log(sayNameAndAge("Thomas", 28));
